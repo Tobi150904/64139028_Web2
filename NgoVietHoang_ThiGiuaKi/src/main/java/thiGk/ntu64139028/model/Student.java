e44b7fbc9744fd144a81c6337ede2164db5b1291
NgoVietHoang <142106010+Tobi150904@users.noreply.github.com>
2025-04-01 09:30:35 +0700

    push lan 3

--- NgoVietHoang_ThiGiuaKi/src/main/java/thiGk/ntu64139028/model/Student.java
+++ NgoVietHoang_ThiGiuaKi/src/main/java/thiGk/ntu64139028/model/Student.java
@@ -45,14 +45,4 @@ public class Student {
 		return "Student [id=" + id + ", name=" + name + ", groupId=" + groupId + "]";
 	}
 	
-	// HashCode
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((groupId == null) ? 0 : groupId.hashCode());
-		result = prime * result + id;
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		return result;
-	}
 }
--- NgoVietHoang_ThiGiuaKi/src/main/java/thiGk/ntu64139028/model/Topic.java
+++ NgoVietHoang_ThiGiuaKi/src/main/java/thiGk/ntu64139028/model/Topic.java
@@ -63,16 +63,5 @@ public class Topic {
 				+ ", supervisorId=" + supervisorId + ", topicType=" + topicType + "]";
 	}
 	
-	// Hashcode 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + id;
-		result = prime * result + ((topicDescription == null) ? 0 : topicDescription.hashCode());
-		result = prime * result + ((topicName == null) ? 0 : topicName.hashCode());
-		result = prime * result + ((topicType == null) ? 0 : topicType.hashCode());
-		result = prime * result + supervisorId;
-		return result;
-	}
+	
 }
